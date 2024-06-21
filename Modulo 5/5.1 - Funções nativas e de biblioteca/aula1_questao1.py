30f4e8241a3ca2973b4813b7dc406cce7be1a23b
rian_carvalho <167252930+rianpjdesenvolve@users.noreply.github.com>
2024-06-20 21:32:19 -0300

    Delete Modulo 5/5.1 - Funções nativas e de biblioteca directory

--- Modulo 5/5.1 - Funções nativas e de biblioteca/aula1_questao1.py
+++
@@ -1,8 +0,0 @@
-# Solicita ao usuário para inserir dois números decimais
-numero1 = float(input("Digite o primeiro número: "))
-numero2 = float(input("Digite o segundo número: "))
-
-diferenca_absoluta = abs(numero1 - numero2)
-diferenca_absoluta_arredondada = round(diferenca_absoluta, 2)
-
-print("A diferença absoluta entre os números é:", diferenca_absoluta_arredondada)
